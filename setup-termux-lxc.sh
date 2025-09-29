95c4e17a3ba00a3f3d3664e002c2599a8cd2d436
eirkkk <33490753+eirkkk@users.noreply.github.com>
2025-09-29 12:34:56 +0300

    Update setup-termux-lxc.sh

--- setup-termux-lxc.sh
+++ setup-termux-lxc.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/env sh
 
 # Some necessary environment variables
-GITHUB_URL="https://github.com/George-Seven/Termux-LXC-Guide"
+GITHUB_URL="https://github.com/eirkkk/Termux-LXC-Guide"
 export HOME="/data/data/com.termux/files/home"
 GITHUB_DIR="${HOME}/$(basename "${GITHUB_URL}")"
 DEPENDENCIES="lxc tsu nano mount-utils pulseaudio termux-tools dos2unix curl git iptables dnsmasq"
